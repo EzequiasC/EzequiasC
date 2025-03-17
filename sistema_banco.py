8eb635eb44609a05a12e40c6f71bfed428480bbe
Ezequias Correa <ezequiascorrea660@gmail.com>
2025-03-17 13:17:19 -0400

    Update sistema_banco.py

--- sistema_banco.py
+++ sistema_banco.py
@@ -23,6 +23,7 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            print(f"O valor foi de {valor} foi creditado.")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -49,6 +50,7 @@ while True:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
+            print(f"O valor de {valor} saiu de sua conta.")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
